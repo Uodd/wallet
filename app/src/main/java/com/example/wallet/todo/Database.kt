f621c9fb8f9dd68a6ae1a76482c2ce952f96f100
uodd <ignaciom9@hotmail.com>
2024-08-24 20:47:47 -0300

    added todo folder

---
+++ app/src/main/java/com/example/wallet/todo/Database.kt
@@ -0,0 +1,16 @@
+package com.example.wallet.todo
+
+
+///Just an idea to improve databases creation
+/*
+class Database<T>(context: Context,serializableColumns:T) {
+    //private var openhelper
+    //private var db
+
+    fun wOpen(){
+
+    }
+    fun rOpen(){
+    }
+
+}*/
