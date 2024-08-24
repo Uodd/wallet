c9e36b4b971a39b8b1cc44e2a820076b03b7d545
uodd <ignaciom9@hotmail.com>
2024-08-24 20:51:51 -0300

    added ActivityData and modified to UserData

--- app/src/main/java/com/example/wallet/Serializables.kt
+++ app/src/main/java/com/example/wallet/Serializables.kt
@@ -1,9 +1,13 @@
 package com.example.wallet
 
 import kotlinx.serialization.Serializable
+import javax.net.ssl.SSLEngineResult.Status
 
 @Serializable
-data class UserData(val id:Int,val firstName:String,val lastName:String)
+data class UserData(val id:Int=-1,val firstName:String,val lastName:String,val time:String)
+
+@Serializable
+data class ActivityData(val id:Int,val userid:Int,val status: Int,val time:String)
 
 @Serializable
 object LogInView
