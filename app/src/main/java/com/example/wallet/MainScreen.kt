e8c0605403e3e9d505a1a9f1f56b797ad2600ce7
uodd <ignaciom9@hotmail.com>
2024-08-25 03:07:14 -0300

    added biometrical autentication and databases instances

--- app/src/main/java/com/example/wallet/MainScreen.kt
+++ app/src/main/java/com/example/wallet/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.example.wallet
 
 import android.content.Context
+import android.view.View.inflate
 import android.widget.Toast
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,21 +9,37 @@ import androidx.core.content.ContextCompat
 import com.example.wallet.ui.theme.WalletTheme
 import java.util.concurrent.Executor
 import androidx.biometric.BiometricPrompt
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.currentComposer
+import androidx.compose.ui.viewinterop.AndroidViewBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import com.example.wallet.db.ActivityDb
 
-private lateinit var executor: Executor
-private lateinit var biometricPrompt: BiometricPrompt
-private lateinit var promptInfo: BiometricPrompt.PromptInfo
+import com.example.wallet.db.CardsDb
+import com.example.wallet.db.UsersDb
+
+
 @Composable
 fun MainScreen(
-    gotoLogin: (String)-> Unit,
-    userid : String?,
-    context: Context
+    gotoLogin: ()-> Unit,
+    userid : String,
+    usersDb:UsersDb,
+    activityDb: ActivityDb,
+    cardsDB: CardsDb
+
 ){
-    //Biometric(context)
-    WalletTheme {
-        Text("USER")
+    BioAuth("Test",userid,gotoLogin)
+
+    val users=usersDb
+    val currentUser= usersDb.getUsers(id=userid.toInt())[0]
+
+    val activities=activityDb
+
+    val cards = cardsDB
+    Column {
+        Text("Hola, ${currentUser.firstName} ${currentUser.lastName}!!")
+        Text("Saldo : ${currentUser.saldo}") //Harcoded at UserData Class, only to show saved data TODO add saveSaldo(userId) to db handler
 
     }
 }
