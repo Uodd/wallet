77c574567d33d09c7449f074138fa5f908de3c1b
uodd <ignaciom9@hotmail.com>
2024-08-25 03:02:50 -0300

    fix: now get key Name and data to encrypt as parameters

--- app/src/main/java/com/example/wallet/Authentication.kt
+++ app/src/main/java/com/example/wallet/Authentication.kt
@@ -1,61 +1,105 @@
 package com.example.wallet;
 
-import android.content.Context;
-import android.hardware.biometrics.BiometricManager;
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
-import java.security.KeyPairGenerator
+import android.util.Log
+import android.widget.Toast
+import androidx.biometric.BiometricPrompt
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+import kotlinx.coroutines.delay
+import java.security.KeyStore
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 
-/*
- * Generate a new EC key pair entry in the Android Keystore by
- * using the KeyPairGenerator API. The private key can only be
- * used for signing or verification and only with SHA-256 or
- * SHA-512 as the message digest.
- */
-class Authentication() {
-    private val parameterSpec: KeyGenParameterSpec = KeyGenParameterSpec.Builder(
-        "mainKey"
-        ,
-        KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
-        .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-        .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
-        .setUserAuthenticationRequired(true)
-        // Invalidate the keys if the user has registered a new biometric
-        // credential, such as a new fingerprint. Can call this method only
-        // on Android 7.0 (API level 24) or higher. The variable
-        // "invalidatedByBiometricEnrollment" is true by default.
-        .setInvalidatedByBiometricEnrollment(true)
+private lateinit var biometricPrompt: BiometricPrompt
+private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+@Composable
+fun BioAuth(data:String,alias:String, exitCallback:()->Unit){
+    Log.v("INFO","Started Encryption")
+
+    val cipher = getCipher()
+    val secretKey =getSecretKey(alias)
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+    val activity = LocalContext.current as FragmentActivity
+    val executor = ContextCompat.getMainExecutor(activity)
+
+    biometricPrompt = BiometricPrompt(activity, executor,
+    object : BiometricPrompt.AuthenticationCallback() {
+        override fun onAuthenticationError(errorCode: Int,
+                                           errString: CharSequence) {
+            exitCallback()
+        }
+
+        override fun onAuthenticationSucceeded(
+            result: BiometricPrompt.AuthenticationResult) {
+            super.onAuthenticationSucceeded(result)
+
+            val encryptedInfo: ByteArray? = result.cryptoObject?.cipher?.doFinal(
+                data.toByteArray()
+            )
+
+            Log.v("MY_APP_TAG", "Encrypted information: " +
+                    encryptedInfo.contentToString()
+            )
+        }
+        override fun onAuthenticationFailed() {
+            exitCallback()
+        }
+    })
+
+    promptInfo = BiometricPrompt.PromptInfo.Builder()
+        .setTitle("Biometrics allow access to secret key")
+        .setSubtitle("Authenticate")
+        .setNegativeButtonText("Exit")
         .build()
-    private val kg = KeyGenerator.getInstance("AES","AndroidKeyStore")
 
-    init {
-        kg.init(parameterSpec)
-        val cipher = getCipher()
-        val secretKey =getSecretKey()
-        cipher.init(Cipher.ENCRYPT_MODE,secretKey)
+    LaunchedEffect(Unit){
+        delay(500)
+        biometricPrompt.authenticate(promptInfo,BiometricPrompt.CryptoObject(cipher))
     }
 
-    private fun getCipher():Cipher{
-        return Cipher.getInstance("AES/CBC/PKCS5PADDING")
-    }
-    private fun getSecretKey():SecretKey{
-        return kg.generateKey()
-    }
+
 
 }
-/*
-generateSecretKey(KeyGenParameterSpec.Builder(
-KEY_NAME,
-KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
-.setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-.setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
-.setUserAuthenticationRequired(true)
-// Invalidate the keys if the user has registered a new biometric
-// credential, such as a new fingerprint. Can call this method only
-// on Android 7.0 (API level 24) or higher. The variable
-// "invalidatedByBiometricEnrollment" is true by default.
-.setInvalidatedByBiometricEnrollment(true)
-.build())
+
+    private fun getCipher():Cipher{
+        return Cipher.getInstance("AES/CBC/PKCS7PADDING")
+    }
+    private fun getSecretKey(alias:String):SecretKey{
+        val keyStore = KeyStore.getInstance("AndroidKeyStore")
+        keyStore.load(null)
+        val key = keyStore.getKey(alias,null)
+        if(key==null){
+            generateSecretKey(alias)
+            return getSecretKey(alias)
+
+        }
+        return  key as SecretKey
+    }
+    private fun generateSecretKey(alias:String){
+        val parameterSpec: KeyGenParameterSpec = KeyGenParameterSpec.Builder(
+            alias
+            ,
+            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+            .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+            .setUserAuthenticationRequired(true)
+            // Invalidate the keys if the user has registered a new biometric
+            // credential, such as a new fingerprint. Can call this method only
+            // on Android 7.0 (API level 24) or higher. The variable
+            // "invalidatedByBiometricEnrollment" is true by default.
+            .setInvalidatedByBiometricEnrollment(true)
+            .build()
+
+        val kg = KeyGenerator.getInstance("AES","AndroidKeyStore")
+        kg.init(parameterSpec)
+        kg.generateKey()
+    }
+
+
