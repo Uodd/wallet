91b0a1c12ee122d8d21c3a5d9b7472795c2975fc
uodd <ignaciom9@hotmail.com>
2024-08-25 03:01:19 -0300

    added db handler to set and get

--- app/src/main/java/com/example/wallet/db/CreditCardsDb.kt
+++ app/src/main/java/com/example/wallet/db/CreditCardsDb.kt
@@ -1,9 +1,15 @@
 package com.example.wallet.db
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.CursorIndexOutOfBoundsException
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
+import android.util.Log
+import com.example.wallet.CREATE_TXT
+import com.example.wallet.CardData
+import com.example.wallet.UserData
 
 
 object CreditCards : BaseColumns {
@@ -13,6 +19,7 @@ object CreditCards : BaseColumns {
     const val COLUMN_EXPIRE = "expire"
     const val COLUMN_USERID = "userid"
     const val COLUMN_PUBLIC = "public"
+    const val COLUMN_BRAND = "brand"
 }
 
 
@@ -23,11 +30,12 @@ private const val SQL_CREATE_CARDS =
             "${CreditCards.COLUMN_SECURE} BLOB," +
             "${CreditCards.COLUMN_EXPIRE} BLOB," +
             "${CreditCards.COLUMN_USERID} BLOB," +
+            "${CreditCards.COLUMN_BRAND} TEXT," +
             "${CreditCards.COLUMN_PUBLIC} TEXT)"
 
 private const val SQL_DELETE_CARDS = "DROP TABLE IF EXISTS ${CreditCards.TABLE_NAME}"
 
-class CreditCardsDb(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class CreditCardsDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(SQL_CREATE_CARDS)
     }
@@ -47,3 +55,62 @@ class CreditCardsDb(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,
     }
 }
 
+class CardsDb(context: Context){
+    private val db = CreditCardsDbHelper(context)
+
+    fun save (card:CardData):Long?{
+        val dbw= db.writableDatabase
+        val values = ContentValues().apply {
+            put(CreditCards.COLUMN_NUMBER,card.number)
+            put(CreditCards.COLUMN_SECURE,card.secure)
+            put(CreditCards.COLUMN_EXPIRE,card.expire)
+            put(CreditCards.COLUMN_PUBLIC,card.public)
+            put(CreditCards.COLUMN_USERID,card.userid)
+            put(CreditCards.COLUMN_BRAND,card.brand)
+        }
+        val rowID= dbw?.insert(Users.TABLE_NAME,null,values)
+        dbw.close()
+        return rowID
+    }
+    fun getCards(userID:Int):MutableList<CardData>{
+        val dbr= db.readableDatabase
+        val cards= mutableListOf<CardData>()
+        try{
+            val selection =  "${CreditCards.COLUMN_USERID}=?"
+            val selectionArgs= arrayOf(userID.toString())
+
+            val cursor =dbr.query(
+                Users.TABLE_NAME,
+                null,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null
+            )
+            try{
+                with(cursor){
+                    while(moveToNext()){
+                        val id = getInt(getColumnIndexOrThrow(BaseColumns._ID))
+                        val number = getBlob(getColumnIndexOrThrow(CreditCards.COLUMN_NUMBER)).toString()
+                        val secure = getBlob(getColumnIndexOrThrow(CreditCards.COLUMN_SECURE)).toString()
+                        val expire = getBlob(getColumnIndexOrThrow(CreditCards.COLUMN_EXPIRE)).toString()
+                        val userid = getInt(getColumnIndexOrThrow(CreditCards.COLUMN_USERID))
+                        val public = getString(getColumnIndexOrThrow(CreditCards.COLUMN_PUBLIC))
+                        val brand = getString(getColumnIndexOrThrow(CreditCards.COLUMN_BRAND))
+
+                        cards.add(CardData(id,number,secure,expire,userid,public,brand))
+                    }
+                }
+            }catch (e:IllegalArgumentException){
+                Log.v("EXCEPTION",e.toString())
+                dbr.close()
+            }
+        }catch (e: CursorIndexOutOfBoundsException){
+            Log.v("EXCEPTION",e.toString())
+            dbr.close()
+        }
+        return cards
+
+    }
+}
