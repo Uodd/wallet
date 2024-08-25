17b9e7168799ebdff4bcf226038e7f70325f2082
uodd <ignaciom9@hotmail.com>
2024-08-24 22:06:33 -0300

    added gotoLogin and context parameters

--- app/src/main/java/com/example/wallet/MainScreen.kt
+++ app/src/main/java/com/example/wallet/MainScreen.kt
@@ -1,17 +1,29 @@
 package com.example.wallet
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.core.content.ContextCompat
 import com.example.wallet.ui.theme.WalletTheme
+import java.util.concurrent.Executor
+import androidx.biometric.BiometricPrompt
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 
+private lateinit var executor: Executor
+private lateinit var biometricPrompt: BiometricPrompt
+private lateinit var promptInfo: BiometricPrompt.PromptInfo
 @Composable
 fun MainScreen(
-    onGOTOCards: ()-> Unit,
-   // user : UserData
+    gotoLogin: (String)-> Unit,
+    userid : String?,
+    context: Context
 ){
+    //Biometric(context)
     WalletTheme {
         Text("USER")
 
     }
-
 }
+
