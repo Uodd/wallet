44ef08c7a66e87cabb87c5455fa758068e7670fd
uodd <ignaciom9@hotmail.com>
2024-08-24 21:01:59 -0300

    implemented userDb read-wwrite

--- app/src/main/java/com/example/wallet/LogInScreen.kt
+++ app/src/main/java/com/example/wallet/LogInScreen.kt
@@ -18,22 +18,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.wallet.ui.theme.WalletTheme
+import com.example.wallet.db.UsersDb
 
 
-val LOG_IN_TXT = "Login as "
-val CREATE_TXT = "Create Account"
-
+const val LOG_IN_TXT = "Login as "
+const val CREATE_TXT = "Create Account"
+const val CLOSE_CARD_TXT= "Close"
 
 @Composable
 fun LogInScreen(
-    onGOTOMain: ()-> Unit
+    gotoMain: (String)-> Unit,
+    gotoLogin: ()->Unit,
+    usersDb: UsersDb
 ){
     var status by remember { mutableStateOf("") }
-
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -44,10 +44,10 @@ fun LogInScreen(
     ) {
         when(status){
             "create" -> {
-                CreateUserCard()
+                CreateUserCard(gotoMain,gotoLogin,usersDb)
             }
             "login" -> {
-                LoginCard()
+                LoginCard(gotoMain,gotoLogin,usersDb)
             }
             else -> {
             }
@@ -80,17 +80,29 @@ fun LogInScreen(
     }
 }
 @Composable
-fun LoginCard(){
+fun LoginCard(gotoMain: (String)-> Unit,
+              gotoLogin: ()->Unit,
+              usersDb: UsersDb
+){
+    Column {
+        //Close
+        Button(onClick =gotoLogin) {
+            Text(text = CLOSE_CARD_TXT)
+        }
+    }
     Text("COMPLETAR")
 }
 
-val FNAME_TEXT = "First Name"
-val LNAME_TEXT = "Last Name"
-val SUBMIT_TEXT = "Submit"
-val CREATE_TITLE_TEXT = "Create User"
+const val FNAME_TEXT = "First Name"
+const val LNAME_TEXT = "Last Name"
+const val SUBMIT_TEXT = "Submit"
+const val CREATE_TITLE_TEXT = "Create User"
 
 @Composable
-fun CreateUserCard(){
+fun CreateUserCard(gotoMain: (String)-> Unit,
+                   gotoLogin: ()->Unit,
+                   usersDb: UsersDb
+){
     var firstname by remember {mutableStateOf("")}
     var lastname by remember { mutableStateOf("")}
     Card(
@@ -99,6 +111,10 @@ fun CreateUserCard(){
             .padding(60.dp),
         ){
         Column {
+            //Close
+            Button(onClick =gotoLogin) {
+                Text(text = CLOSE_CARD_TXT)
+            }
             //Title
             Text(CREATE_TITLE_TEXT,
                 modifier = Modifier
@@ -119,7 +135,22 @@ fun CreateUserCard(){
                     LNAME_TEXT)} )
             OutlinedButton(
                 modifier = Modifier.padding(top = 30.dp),
-                onClick = {  }
+                onClick = {
+                    var userId: Long?
+                    //Saves UserData TODO Ask for input if needed! Add another characters checks
+                    if (firstname!="" && lastname!="") {
+                        userId = usersDb.save(
+                            UserData(
+                                firstName = firstname,
+                                lastName = lastname,
+                                time = getTimeString()
+                            )
+                        )
+                        if(userId!=null){gotoMain(userId.toString())}
+                        else{gotoLogin()}
+
+                    }
+                }
             ){
                 Text(
                     text =SUBMIT_TEXT,
