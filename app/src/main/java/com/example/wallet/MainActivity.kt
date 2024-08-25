cec35f23aafaeb3ade2be205a8aa276121fb6f0d
uodd <ignaciom9@hotmail.com>
2024-08-25 03:04:21 -0300

    modified main class parent to use authentication prompt

--- app/src/main/java/com/example/wallet/MainActivity.kt
+++ app/src/main/java/com/example/wallet/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.fragment.app.FragmentActivity
 
 
-
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //this.deleteDatabase("ActivityData.db") //Debugging databases... deletes db file
+        //this.deleteDatabase("UsersData.db") //Debugging databases... deletes db file
         enableEdgeToEdge()
         setContent {
             WalletApp(this)
