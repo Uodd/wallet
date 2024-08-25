ac68495cf5f1c8ba4561c07e3f0dc5d478dfc722
uodd <ignaciom9@hotmail.com>
2024-08-25 13:07:34 -0300

    now blocks MainScreen until Authenticated

--- app/src/main/java/com/example/wallet/MainScreen.kt
+++ app/src/main/java/com/example/wallet/MainScreen.kt
@@ -1,24 +1,23 @@
 package com.example.wallet
-
-import android.content.Context
-import android.view.View.inflate
-import android.widget.Toast
+import android.util.Log
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.core.content.ContextCompat
-import com.example.wallet.ui.theme.WalletTheme
-import java.util.concurrent.Executor
-import androidx.biometric.BiometricPrompt
+
 import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.currentComposer
-import androidx.compose.ui.viewinterop.AndroidViewBinding
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 import com.example.wallet.db.ActivityDb
 
 import com.example.wallet.db.CardsDb
 import com.example.wallet.db.UsersDb
-
 
 @Composable
 fun MainScreen(
@@ -29,18 +28,92 @@ fun MainScreen(
     cardsDB: CardsDb
 
 ){
-    BioAuth("Test",userid,gotoLogin)
+    val TAG="MAIN SCREEN"
+    Log.v(TAG,"UserId at Main $userid")
 
-    val users=usersDb
-    val currentUser= usersDb.getUsers(id=userid.toInt())[0]
-
-    val activities=activityDb
-
-    val cards = cardsDB
-    Column {
-        Text("Hola, ${currentUser.firstName} ${currentUser.lastName}!!")
-        Text("Saldo : ${currentUser.saldo}") //Harcoded at UserData Class, only to show saved data TODO add saveSaldo(userId) to db handler
-
+    var status by remember { mutableStateOf("") }
+    fun setStatus(s:String){
+        status=s
     }
+
+    when(status){
+        "ok" -> {
+            Authenticated(
+                gotoLogin = gotoLogin,
+                userid = userid ,
+                usersDb = usersDb ,
+                activityDb = activityDb,
+                cardsDB = cardsDB
+            )
+        }
+        "else" -> {
+            Text(text = "Authenticate!",Modifier.padding(100.dp))
+        }
+        else -> {
+        }
+    }
+    AutenticateMain(successCallback = { setStatus("ok") }, exitCallback = { setStatus("no") })
+
 }
 
+@Composable
+fun Authenticated(gotoLogin: ()-> Unit,
+                  userid : String,
+                  usersDb:UsersDb,
+                  activityDb: ActivityDb,
+                  cardsDB: CardsDb){
+    val currentUser= usersDb.getUsers(ID=userid.toInt())[0]
+    //var qr by remember { mutableStateOf("") }
+    activityDb.save(ActivityData(userid = currentUser.id, status = 1,time= getTimeString())) //Save connection activity TODO addDisconnection
+    val cards = cardsDB.getCards(currentUser.id)
+    for(c in cards){
+        Log.v("AAAA","$c")
+    }
+    Column {
+        Text("Hola, ${currentUser.firstName} ${currentUser.lastName}!!")
+        Text("Saldo : ${currentUser.saldo}") //Hardcoded at UserData Class, only to show saved data TODO add saveSaldo(userId) to db handler
+        Button(onClick = { }) {
+            Text(text = "Generate QR")
+        }
+        CardsPreview(cards = cards, currentUser = currentUser )
+
+        Button(onClick = {}) {
+            Text(text = "Add Credit Card")
+        }
+//        when(status){
+//            "addcard"->{
+//
+//            }
+//            "qr"->{
+//
+//            }else->{
+//
+//            }
+//        }
+    }
+}
+@Composable
+fun CardsPreview(cards:MutableList<CardData>,currentUser:UserData) {
+    for(c in cards){
+        CardWidget(card = c, currentUser = currentUser )
+    }
+}
+val brandNum= mapOf("3" to "amex", "4" to "visa", "5" to "mastercard" )
+
+@Composable
+fun CardWidget(card:CardData,currentUser:UserData){
+    Card(modifier = Modifier.padding(20.dp),
+        onClick = {//TODO GOTO PAY
+
+        }){
+        var brand ="Unknow"
+        if(brandNum.containsKey(card.brand)){
+            brand = brandNum[card.brand].toString()
+        }
+        Text(text ="Last 4: $card.public. Brand:$brand Name:${currentUser.firstName} ${currentUser.lastName}" ,Modifier.padding(10.dp))
+    }
+}
+@Composable
+fun AddCreditCard(currentUser: UserData){
+
+}
