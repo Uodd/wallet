6cae49e30c8ccc5d898f6f9d8bc33e90652ff728
uodd <ignaciom9@hotmail.com>
2024-08-25 03:06:12 -0300

    updated base data classes

--- app/src/main/java/com/example/wallet/Serializables.kt
+++ app/src/main/java/com/example/wallet/Serializables.kt
@@ -1,13 +1,14 @@
 package com.example.wallet
 
 import kotlinx.serialization.Serializable
+import org.w3c.dom.Text
 import javax.net.ssl.SSLEngineResult.Status
 
 @Serializable
-data class UserData(val id:Int=-1,val firstName:String,val lastName:String,val time:String)
+data class UserData(val id:Int=-1,val firstName:String,val lastName:String,val saldo:Float=0.toFloat(), val time:String)
 
 @Serializable
-data class ActivityData(val id:Int,val userid:Int,val status: Int,val time:String)
+data class ActivityData(val id:Int=-1,val userid:Int,val status: Int,val time:String)
 
 @Serializable
-object LogInView
+data class CardData(val id:Int=-1,val number:String,val secure:String,val expire:String,val userid:Int, val public:String,val brand:String)
