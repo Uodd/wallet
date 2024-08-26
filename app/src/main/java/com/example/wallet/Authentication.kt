96715273f91746b7fc08e9fcfb6133563e538b3e
uodd <ignaciom9@hotmail.com>
2024-08-26 03:24:42 -0300

    added add new Card, reworked biometrical encryption

--- app/src/main/java/com/example/wallet/Authentication.kt
+++ app/src/main/java/com/example/wallet/Authentication.kt
@@ -3,21 +3,19 @@ package com.example.wallet;
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import android.util.Log
-import android.widget.Toast
 import androidx.biometric.BiometricPrompt
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
-import com.example.wallet.db.ActivityDb
-import com.example.wallet.db.CardsDb
-import com.example.wallet.db.UsersDb
 import kotlinx.coroutines.delay
 import java.security.KeyStore
+import java.security.SecureRandom
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 
 private lateinit var biometricPrompt: BiometricPrompt
 private lateinit var promptInfo: BiometricPrompt.PromptInfo
@@ -25,59 +23,66 @@ private lateinit var promptInfo: BiometricPrompt.PromptInfo
 val TAG = "AUTHENTICATION"
 
 @Composable
-fun AutenticateMain(successCallback:()->Unit,
-                    exitCallback: () -> Unit,
-                    keyalias: String ="dummy"
-){
-    Log.v(TAG,"Started Encryption")
+fun AuthenticateMain(
+    successCallback: () -> Unit,
+    exitCallback: () -> Unit,
+    keyalias: String = "dummy" //maybe this variable could be used to add another encryption layer?
+) {
+    Log.v(TAG, "Started Encryption")
     val cipher = prepareEncrypt(keyalias)
     val activity = LocalContext.current as FragmentActivity
     val executor = ContextCompat.getMainExecutor(activity)
 
-    LaunchedEffect(Unit){
+    LaunchedEffect(Unit) {
         delay(500)
-        biometricPrompt.authenticate(promptInfo,BiometricPrompt.CryptoObject(cipher))
+        biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
     }
 
     promptInfo = BiometricPrompt.PromptInfo.Builder()
         .setTitle("Authenticate to Start")
-        .setSubtitle("This App Secures (?) your data")
+        .setSubtitle("This App secures (?) your data")
         .setNegativeButtonText("Exit")
         .build()
 
     biometricPrompt = BiometricPrompt(activity, executor,
         object : BiometricPrompt.AuthenticationCallback() {
-            override fun onAuthenticationError(errorCode: Int,
-                                               errString: CharSequence) {
+            override fun onAuthenticationError(
+                errorCode: Int,
+                errString: CharSequence
+            ) {
                 exitCallback()
             }
 
             override fun onAuthenticationSucceeded(
-                result: BiometricPrompt.AuthenticationResult) {
+                result: BiometricPrompt.AuthenticationResult
+            ) {
                 super.onAuthenticationSucceeded(result)
                 successCallback()
                 Log.v(TAG, "Encrypted information")
             }
+
             override fun onAuthenticationFailed() {
                 exitCallback()
             }
         })
 }
+
 @Composable
-fun BioEncryptDataRow(successCallback:(MutableList<ByteArray>)->Unit,
-                      exitCallback: () -> Unit,
-                      data:MutableList<ByteArray> = mutableListOf<ByteArray>(),
-                      keyalias:String = "dummy"
-                    )
-{
-    Log.v(TAG,"Started Encryption")
-    val cipher = prepareEncrypt(keyalias)
+fun BioEncryptData(
+    doneCallback:(ByteArray?) -> Unit,
+    exitCallback: () -> Unit,
+    data: ByteArray ,
+    keyalias: String = "dummy",
+    decrypt: Boolean =false
+) {
+    Log.v(TAG, "Started Encryption")
+    val cipher = if(!decrypt) prepareEncrypt(keyalias) else prepareDecrypt(keyalias)
     val activity = LocalContext.current as FragmentActivity
     val executor = ContextCompat.getMainExecutor(activity)
 
-    LaunchedEffect(Unit){
+    LaunchedEffect(Unit) {
         delay(500)
-        biometricPrompt.authenticate(promptInfo,BiometricPrompt.CryptoObject(cipher))
+        biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
     }
 
     promptInfo = BiometricPrompt.PromptInfo.Builder()
@@ -88,21 +93,24 @@ fun BioEncryptDataRow(successCallback:(MutableList<ByteArray>)->Unit,
 
     biometricPrompt = BiometricPrompt(activity, executor,
         object : BiometricPrompt.AuthenticationCallback() {
-            override fun onAuthenticationError(errorCode: Int,
-                                               errString: CharSequence) {
+            override fun onAuthenticationError(
+                errorCode: Int,
+                errString: CharSequence
+            ) {
                 exitCallback()
             }
 
             override fun onAuthenticationSucceeded(
-                result: BiometricPrompt.AuthenticationResult) {
-                super.onAuthenticationSucceeded(result)
-                val encryptedData= mutableListOf<ByteArray>()
-                for(d in data){
-                    val encryptedD = result.cryptoObject?.cipher?.doFinal(d)
-                    encryptedData.add(encryptedD!!)
-                }
-                successCallback(encryptedData)
-                Log.v(TAG, "Encrypted information")
+                result: BiometricPrompt.AuthenticationResult
+            ) {
+                //val encryptedData = mutableListOf<ByteArray>()
+
+                //it is not possible for now to encrypt multiple times with one prompt. Superchunk blends all data in one piece to chopp when decrypted
+                val encryptedD = result.cryptoObject?.cipher?.doFinal(data)
+                Log.v(TAG,"DATA: $data")
+                Log.v(TAG, "${if(decrypt) "De" else "En"}crypted information $encryptedD")
+                doneCallback(encryptedD)
+
             }
             override fun onAuthenticationFailed() {
                 exitCallback()
@@ -110,34 +118,40 @@ fun BioEncryptDataRow(successCallback:(MutableList<ByteArray>)->Unit,
         })
 }
 
-fun prepareEncrypt(keyalias:String):Cipher{
+fun prepareEncrypt(keyalias: String): Cipher {
     val cipher = getCipher()
-    val secretKey =getSecretKey(keyalias)
+    val secretKey = getSecretKey(keyalias)
     cipher.init(Cipher.ENCRYPT_MODE, secretKey)
     return cipher
 }
+fun prepareDecrypt(keyalias: String):Cipher{
+    val cipher = getCipher()
+    val secretKey = getSecretKey(keyalias)
+    cipher.init(Cipher.DECRYPT_MODE, secretKey)
+    return cipher
+}
 
-private fun getCipher():Cipher{
+private fun getCipher(): Cipher {
     return Cipher.getInstance("AES/CBC/PKCS7PADDING")
 }
 
-private fun getSecretKey(alias:String):SecretKey{
+private fun getSecretKey(alias: String): SecretKey {
     val keyStore = KeyStore.getInstance("AndroidKeyStore")
     keyStore.load(null)
-    val key = keyStore.getKey(alias,null)
-    if(key==null){
+    val key = keyStore.getKey(alias, null)
+    if (key == null) {
         generateSecretKey(alias)
         return getSecretKey(alias)
 
     }
-    return  key as SecretKey
+    return key as SecretKey
 }
 
-private fun generateSecretKey(alias:String){
+private fun generateSecretKey(alias: String) {
     val parameterSpec: KeyGenParameterSpec = KeyGenParameterSpec.Builder(
-        alias
-        ,
-        KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+        alias,
+        KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+    )
         .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
         .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
         .setUserAuthenticationRequired(true)
@@ -148,9 +162,15 @@ private fun generateSecretKey(alias:String){
         .setInvalidatedByBiometricEnrollment(true)
         .build()
 
-    val kg = KeyGenerator.getInstance("AES","AndroidKeyStore")
+    val kg = KeyGenerator.getInstance("AES", "AndroidKeyStore")
     kg.init(parameterSpec)
     kg.generateKey()
 }
-
+/////////////////////////////////////////////////////////////////// WORKARROUND NOT WORKING
+//private fun reLoadCipher(cipher:Cipher,alias: String){
+//    val r = SecureRandom()
+//    val ivBytes = ByteArray(16)
+//    r.nextBytes(ivBytes)
+//    cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(alias),IvParameterSpec(ivBytes))
+//}
 
--- app/src/main/java/com/example/wallet/MainScreen.kt
+++ app/src/main/java/com/example/wallet/MainScreen.kt
@@ -1,5 +1,8 @@
 package com.example.wallet
+
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
@@ -7,113 +10,310 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.currentComposer
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.unit.dp
 
 import com.example.wallet.db.ActivityDb
 
 import com.example.wallet.db.CardsDb
 import com.example.wallet.db.UsersDb
+import kotlinx.coroutines.time.delay
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
+import kotlin.time.Duration
+
+
+const val MS = "MainScreen"
+val toEncrypt = mutableListOf<ByteArray>()
+val encrypted = mutableListOf<ByteArray>()
+
 
 @Composable
 fun MainScreen(
-    gotoLogin: ()-> Unit,
-    userid : String,
-    usersDb:UsersDb,
+    gotoLogin: () -> Unit,
+    userid: String,
+    usersDb: UsersDb,
     activityDb: ActivityDb,
-    cardsDB: CardsDb
-
-){
-    val TAG="MAIN SCREEN"
-    Log.v(TAG,"UserId at Main $userid")
+    cardsDB: CardsDb,
+    context: Context
+) {
+    val TAG = "MAIN SCREEN"
+    Log.v(TAG, "UserId at Main $userid")
 
     var status by remember { mutableStateOf("") }
-    fun setStatus(s:String){
-        status=s
+    fun setStatus(s: String) {
+        status = s
     }
 
-    when(status){
+
+    fun encryptor(data: ByteArray) {//TODO rework idea to make reusable for other objects
+        toEncrypt.clear()
+        Log.v("ENCRYPTOR", "$data")
+        toEncrypt.add(data)
+        Log.v("ENCRYPTOR2", "$toEncrypt")
+        setStatus("toEncrypt")
+
+    }
+    AuthenticateMain(successCallback = { setStatus("ok") }, exitCallback = { setStatus("no") })
+    when (status) {
         "ok" -> {
             Authenticated(
                 gotoLogin = gotoLogin,
-                userid = userid ,
-                usersDb = usersDb ,
+                userid = userid,
+                usersDb = usersDb,
                 activityDb = activityDb,
-                cardsDB = cardsDB
+                cardsDB = cardsDB,
+                context = context,
+                setStatus = { s -> status = s }
             )
         }
-        "else" -> {
-            Text(text = "Authenticate!",Modifier.padding(100.dp))
+
+        "addCard" -> {
+            AddCreditCard(
+                encryptor = { data: ByteArray -> encryptor(data) },
+                context = context,
+                currentUser = usersDb.getUsers(userid.toInt())[0]
+            )
+        }
+
+        "toLogin" -> {
+            gotoLogin()
+        }
+        "toEncrypt" -> {
+            Log.v(TAG, "LLEG")
+            BioEncryptData(doneCallback = { d: ByteArray? ->
+                if (d != null) {
+                    encrypted.add(d)
+                    setStatus("toSaveEncrypted")
+                }
+            }, exitCallback = { gotoLogin() }, toEncrypt.last(), userid)
+        }
+        "toSaveEncrypted" -> {
+            Log.v("To Encrypt",encrypted.last().toString(Charset.defaultCharset()))
+            Log.v("SAVE ENCRYPTED", "$encrypted")
+            //save encrypted
+            encrypted.clear()
+            setStatus("ok")
+        }
+        "toShowQR"->{
+
         }
         else -> {
+            Text(text = "Authenticate!", Modifier.padding(100.dp))
         }
     }
-    AutenticateMain(successCallback = { setStatus("ok") }, exitCallback = { setStatus("no") })
-
 }
 
+val ADD_CARDT_TXT = "Add Card"
+
 @Composable
-fun Authenticated(gotoLogin: ()-> Unit,
-                  userid : String,
-                  usersDb:UsersDb,
-                  activityDb: ActivityDb,
-                  cardsDB: CardsDb){
-    val currentUser= usersDb.getUsers(ID=userid.toInt())[0]
-    //var qr by remember { mutableStateOf("") }
-    activityDb.save(ActivityData(userid = currentUser.id, status = 1,time= getTimeString())) //Save connection activity TODO addDisconnection
+fun Authenticated(
+    gotoLogin: () -> Unit,
+    userid: String,
+    usersDb: UsersDb,
+    activityDb: ActivityDb,
+    cardsDB: CardsDb,
+    context: Context,
+    setStatus: (String) -> Unit
+) {
+
+    val currentUser = usersDb.getUsers(ID = userid.toInt())[0]
+    activityDb.save(
+        ActivityData(
+            userid = currentUser.id,
+            status = 1,
+            time = getTimeString()
+        )
+    ) //SaveS connection activity TODO addDisconnection
+
     val cards = cardsDB.getCards(currentUser.id)
-    for(c in cards){
-        Log.v("AAAA","$c")
-    }
+
+
     Column {
+        Button(onClick = {
+            gotoLogin()
+            activityDb.save(
+                ActivityData(
+                    userid = currentUser.id,
+                    status = 0,
+                    time = getTimeString()
+                )
+            )
+        }) { Text(text = "LogOut") }
         Text("Hola, ${currentUser.firstName} ${currentUser.lastName}!!")
         Text("Saldo : ${currentUser.saldo}") //Hardcoded at UserData Class, only to show saved data TODO add saveSaldo(userId) to db handler
-        Button(onClick = { }) {
-            Text(text = "Generate QR")
-        }
-        CardsPreview(cards = cards, currentUser = currentUser )
+        Button(onClick = {
+            //TODO http request and handle response!
 
-        Button(onClick = {}) {
-            Text(text = "Add Credit Card")
-        }
-//        when(status){
-//            "addcard"->{
-//
-//            }
-//            "qr"->{
-//
-//            }else->{
-//
-//            }
-//        }
+        }, modifier = Modifier.padding(20.dp)) { Text(text = "Generate QR") }
+        Button(
+            onClick = { setStatus("addCard") },
+            modifier = Modifier.padding(20.dp)
+        ) { Text(text = "Add Card") }
     }
 }
-@Composable
-fun CardsPreview(cards:MutableList<CardData>,currentUser:UserData) {
-    for(c in cards){
-        CardWidget(card = c, currentUser = currentUser )
-    }
-}
-val brandNum= mapOf("3" to "amex", "4" to "visa", "5" to "mastercard" )
 
 @Composable
-fun CardWidget(card:CardData,currentUser:UserData){
+fun CardsPreview(cards: MutableList<CardData>, currentUser: UserData) {
+
+    for (c in cards) {
+        CardWidget(card = c, currentUser = currentUser)
+    }
+}
+
+val brandNum = mapOf("3" to "amex", "4" to "visa", "5" to "mastercard")
+
+@Composable
+fun CardWidget(card: CardData, currentUser: UserData) {
     Card(modifier = Modifier.padding(20.dp),
         onClick = {//TODO GOTO PAY
 
-        }){
-        var brand ="Unknow"
-        if(brandNum.containsKey(card.brand)){
+        }) {
+        var brand = "Unknow"
+        if (brandNum.containsKey(card.brand)) {
             brand = brandNum[card.brand].toString()
         }
-        Text(text ="Last 4: $card.public. Brand:$brand Name:${currentUser.firstName} ${currentUser.lastName}" ,Modifier.padding(10.dp))
+        Text(
+            text = "Last 4: $card.public. Brand:$brand Name:${currentUser.firstName} ${currentUser.lastName}",
+            Modifier.padding(10.dp)
+        )
     }
 }
-@Composable
-fun AddCreditCard(currentUser: UserData){
 
+const val FN = "First Name"
+const val LN = "Last Name"
+const val NUM = "Card Number"
+const val SECURE = "Security Numbers"
+const val EXP = "Expiration Date"
+
+@Composable
+fun AddCreditCard(
+    encryptor: (ByteArray) -> Unit,
+    context: Context,
+    currentUser: UserData
+) {
+    var firstname by remember { mutableStateOf("") }
+    var lastname by remember { mutableStateOf("") }
+    var number by remember { mutableStateOf("") }
+    var secure by remember { mutableStateOf("") }
+    var expire by remember { mutableStateOf("") }
+    val modifier = Modifier.padding(10.dp)
+
+    Column {
+        OutlinedTextField(
+            value = firstname,
+            onValueChange = { firstname = it },
+            label = {
+                Text(
+                    FN
+                )
+            }, modifier = modifier
+        )
+        OutlinedTextField(
+            value = lastname,
+            onValueChange = { lastname = it },
+            label = {
+                Text(
+                    LN
+                )
+            }, modifier = modifier
+        )
+        OutlinedTextField(
+            value = number,
+            onValueChange = { number = it },
+            label = {
+                Text(
+                    NUM
+                )
+            }, modifier = modifier
+        )
+        OutlinedTextField(
+            value = secure,
+            onValueChange = { secure = it },
+            label = {
+                Text(
+                    SECURE
+                )
+            }, modifier = modifier
+        )
+        OutlinedTextField(
+            value = expire,
+            onValueChange = { expire = it },
+            label = {
+                Text(
+                    EXP
+                )
+            }, modifier = modifier
+        )
+        OutlinedButton(
+            modifier = modifier,
+            onClick = {
+
+                if (firstname != currentUser.firstName || lastname != currentUser.lastName) {
+                    Toast.makeText(
+                        context,
+                        "Names must be equal to user names!!",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                    Log.v(MS, "BAD NAME INPUTS AT CREATE CARD")
+                } else if (number[0] == '3') { //amex
+                    if (number.length != 15 || secure.length != 4) {
+                        Toast.makeText(context, "WRONG NUMBER OR SECURE NUMBER", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                } else if (number.length != 16 || secure.length != 3) {
+                    Toast.makeText(context, "WRONG NUMBER OR SECURE NUMBER", Toast.LENGTH_SHORT)
+                        .show()
+                } else if (expire.length != 4) {
+                    Toast.makeText(context, "WRONG EXPIRE DATE NEEDS 4 DIGITS", Toast.LENGTH_SHORT)
+                        .show()
+
+                } else if (firstname == "" || lastname == "" || number == "" || secure == "" || expire == "") {
+                    Toast.makeText(context, "Complete all fields!", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+
+                    val data = number + secure + expire
+
+//                        currentUser.id.toString(),
+//                        number.slice(0..4),
+//                        number[0].toString()
+                //TODO slice encrypted superchunk to populate evenly sensible data columns, add the public data to that row. Then sum chunks , decrypt and slice data
+
+                    val bdata=data.toByteArray()
+
+                    Log.v(MS, "DATA: ${data} ")
+                    encryptor(bdata)
+                }
+
+            }
+        ) {
+            Text(
+                text = SUBMIT_TEXT,
+                style = MaterialTheme.typography.labelMedium
+            )
+        }
+        //description text
+        Text(
+            text = """
+                First Name:$firstname
+                Last Name: $lastname
+                Number: $number
+                Secure: $secure
+                Expires: ${expire}
+                Brand: ${if (number.isNotEmpty()) (brandNum.get(number[0].toString()) ?: "unknown") else ""}
+            """.trimIndent(), modifier = modifier
+        )
+    }
 }
